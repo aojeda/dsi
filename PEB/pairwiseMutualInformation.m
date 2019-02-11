7198bbd504f00e2a19ccfe3f172069cab42bd2e7
aojeda <alejo.ojeda83@gmail.com>
2019-02-11 00:02:51 -0800

    Bug fixed

--- PEB/pairwiseMutualInformation.m
+++ PEB/pairwiseMutualInformation.m
@@ -6,6 +6,6 @@ ind = triu(ones(n));
 [indi,indj] = ind2sub([n n],find(ind(:)));
 n = length(indi);
 for k=1:n
-    I(indi(k), indj(k)) = mi(X(indi(k),:),X(indj(k),:));
+    I(indi(k), indj(k)) = mutualInformation(X(indi(k),:),X(indj(k),:));
     I(indj(k), indi(k)) = I(indi(k), indj(k));
 end
