4f67a405d1c70a12286a706a923fdfd3b533f21f
aojeda <alejo.ojeda83@gmail.com>
2019-02-11 00:05:18 -0800

    Adding JIDT library and MI and TE wrappers

--- PEB/mutualInformation.m
+++ PEB/mutualInformation.m
@@ -1,83 +1,60 @@
-function I = mutualInformation(x,y, pdfType)
-% Compute the mutual information (in nats) between continuous random variables x and
-% y.
+function I = mutualInformation(x,y,normalize,k)
+% Use the JIDT library to compute Kraskov's estimate of mutual information 
+% between continuous random variables x and y.
 %
-% Example:
-%  Two variables that share very little information:
+% For this function to work it should be located next to infodynamics.jar, 
+% otherwise download the jar file from: http://jlizier.github.io/jidt/
+%
+%% Example:
+%%  Two variables that share very little information:
 %   x=randn(100000,1);
 %   y=randn(size(x));
 %   I = mutualInformation(x,y);
 %   r = corr(x,y);
 %   disp([I r])
 %
-%  Two variables that share information in a complicated manner:
+%%  Two variables that share information in a complicated manner:
 %   x=randn(100000,1);
 %   y=x.^2;
 %   I = mutualInformation(x,y);
 %   r = corr(x,y);
 %   disp([I r])
 %
-%  Two variables that are linearly related:
+%%  Two variables that are linearly related:
 %   x=randn(100000,1);
 %   y=x+0.1*randn(100000,1);
-%   I = mutualInformation(x,y)
+%   I = mutualInformation(x,y);
 %   r = corr(x,y);
 %   disp([I r])
 %
-% Author: Alejandro Ojeda, Neural Engineering and Translation Labs, University of California San Diego, 2019
+% If you use this function for a publication please cite as:
+% Joseph T. Lizier, "JIDT: An information-theoretic toolkit for studying 
+% the dynamics of complex systems", Frontiers in Robotics and AI 1:11, 2014;
+% doi:10.3389/frobt.2014.00011 (pre-print: arXiv:1408.3270)
+%
+% Author of the wrapper: Alejandro Ojeda, Neural Engineering and Translation
+%                        Labs, University of California San Diego, 2019
 
-if nargin < 3, pdfType = 'ksdensity';end
-if ~any(ismember({'ksdensity','hist'},lower(pdfType)))
-    pdfType = 'ksdensity';
+persistent miCalc
+if isempty(miCalc)
+    p = fileparts(which('mutualInformation.m'));
+    javaaddpath(fullfile(p, 'infodynamics.jar'));
+    miCalc=javaObject('infodynamics.measures.continuous.kraskov.MutualInfoCalculatorMultiVariateKraskov2');
+    miCalc.initialise(1,1); % univariate calculation
 end
-
-x = x/(eps+std(x(:)));
-y = y/(eps+std(y(:)));
-if strcmpi(pdfType,'ksdensity')
-    [px, xi]   = ksdensity(x);
-    [py, yi]   = ksdensity(y);
-    [pxy, xyi] = ksdensity([x(:),y(:)]);
+if nargin < 3
+    normalize = false;    % Set to true to zscore input signals
+end
+if nargin < 4
+    k=4;                    % Nearest neighbours for KSG estimator
+end
+if normalize
+    normalize = 'true';
 else
-    [px, xi]   = hist(x,100);
-    [py, yi]   = hist(y,100);
-    [pxy, xyi] = hist3([x(:),y(:)],[30 30]);
-    pxy = pxy(:);
-    [Sx,Sy] = meshgrid(xyi{1},xyi{2});
-    xyi = [Sx(:),Sy(:)];
+    normalize = 'false';
 end
-
-% Evaluate pdfs in the common support
-n = 100;
-cs = unique([xi yi]);
-cs = linspace(min(cs),max(cs),n);
-fx = griddedInterpolant(xi,px);
-fy = griddedInterpolant(yi,py);
-px = fx(cs);px(px<eps)=0;
-py = fy(cs);py(py<eps)=0;
-
-nxy = sqrt(length(pxy));
-pxy = reshape(pxy,[nxy,nxy]);
-X = reshape(xyi(:,1),[nxy nxy]);
-Y = reshape(xyi(:,2),[nxy nxy]);
-[Sx,Sy] = meshgrid(cs);
-pxy = interp2(X,Y,pxy,Sx,Sy,'linear',0);
-pxy(pxy<eps) = 0;
-
-% Normalize the pdfs
-px = px/nansum(px);
-py = py/nansum(py);
-pxy = pxy/nansum(pxy);
-
-% Compute MI as: sum_x sum_y Pxy*(log(Pxy)-log(Px)-log(Py))
-logPxy = log(pxy);
-logPxPy = bsxfun(@plus,log(px'),log(py));
-logPxy(isinf(logPxy)) = min(logPxy(~isinf(logPxy)));
-logPxPy(isinf(logPxPy)) = min(logPxPy(~isinf(logPxPy)));
-I = pxy.*(logPxy - logPxPy);
-I(isinf(I)) = nan;
-dx = cs(2)-cs(1);
-dy = cs(2)-cs(1);
-I = nansum(I(:)*dy*dx);
-if I<0 && strcmpi(pdfType,'hist')
-    I = mutualInformation(x,y, 'ksdensity');
+miCalc.setProperty('k', num2str(k)); 
+miCalc.setProperty('NORMALISE', normalize);
+miCalc.setObservations(x, y);
+I = miCalc.computeAverageLocalOfObservations();
 end
