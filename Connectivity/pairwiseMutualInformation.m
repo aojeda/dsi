bff5880120d62d9f941ed1c689f3376ed21ae5a4
aojeda <alejo.ojeda83@gmail.com>
2019-02-14 17:29:42 -0800

    Bug fixed

--- Connectivity/pairwiseMutualInformation.m
+++ Connectivity/pairwiseMutualInformation.m
@@ -1,11 +1,11 @@
-function I = pairwiseMutualInformation(X)
+function [I,ind,indi,indj] = pairwiseMutualInformation(X)
 X = X(:,:);
-n = size(X,1);
+n = size(X,2);
 I = zeros(n);
-ind = triu(ones(n));
+ind = triu(ones(n),1);
 [indi,indj] = ind2sub([n n],find(ind(:)));
 n = length(indi);
 for k=1:n
-    I(indi(k), indj(k)) = mutualInformation(X(indi(k),:),X(indj(k),:));
+    I(indi(k), indj(k)) = mutualInformation(X(:,indi(k)),X(:,indj(k)));
     I(indj(k), indi(k)) = I(indi(k), indj(k));
 end
