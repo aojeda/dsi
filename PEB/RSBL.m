702a8ea068d136004c9f69f197387a4c9a8d408d
aojeda <alejo.ojeda83@gmail.com>
2019-08-05 13:50:18 -0700

    Addign RSBL code

---
+++ PEB/RSBL.m
@@ -0,0 +1,280 @@
+classdef RSBL < handle
+    properties
+        H
+        Delta
+        Blocks
+        Ny
+        Nx
+        Ng
+        K
+        defaultOptions = struct(...
+            'maxIter',100,...       % Maximum number of iterations
+            'verbose',true,...      % Produce per-iteration prints
+            'maxTol',1e-1,...       % Maximum tolerance of logE change
+            'bufferSize',100,...    % History buffer size
+            'doPruning','true',...  % Enable the pruning stage
+            'learnLambda','true',...% Enable the pruning stage
+            'learnGammaF','true');  % Enable the pruning stage
+    end
+    properties(GetAccess=private)
+        Hi
+        HiHit
+        CiHt
+        Ci
+        Ut
+        s2
+        Iy
+    end
+    
+    methods
+        function obj = RSBL(H, Delta, blocks)
+            obj.H = H;
+            obj.Delta = Delta;
+            obj.Blocks = blocks;
+            [obj.Ny,obj.Nx] = size(H);
+            obj.Ng = size(blocks,2);     
+            obj.Hi    = cell(1,obj.Ng);
+            obj.HiHit = zeros([obj.Ny, obj.Ny, obj.Ng]);
+            obj.Ci    = sparse(obj.Nx^2,obj.Ng);
+            obj.CiHt  = sparse(obj.Nx*obj.Ny,obj.Ng);
+            ind1      = zeros(obj.Nx,obj.Nx);
+            ind2      = zeros(obj.Nx,obj.Ny);
+            obj.Iy    = speye(obj.Ny);
+            
+            fprintf('Precomputing  ');
+            for k=1:obj.Ng
+                % Per-block square root precision matrix
+                Di = Delta(blocks(:,k),blocks(:,k));
+                
+                % Per-block covariance matrix
+                sqCi_k = inv(Di);
+                ind1(blocks(:,k),blocks(:,k)) = 1;
+                ind2(blocks(:,k),:) = 1;
+                C_i = sqCi_k*sqCi_k';
+                obj.Ci(ind1==1,k) = C_i(:);
+                CiHt_k = C_i*obj.H(:,blocks(:,k))';
+                obj.CiHt(ind2==1,k) = CiHt_k(:);
+                ind2(blocks(:,k),:) = 0;
+                ind1(blocks(:,k),blocks(:,k)) = 0;
+                
+                % Per-block standardized gain matrices
+                obj.Hi{k} = obj.H(:,blocks(:,k))/Di;
+                obj.HiHit(:,:,k) = obj.Hi{k}*obj.Hi{k}';
+                
+                if ~mod(k,round(obj.Ng/10)) 
+                    fprintf('%i%% ',round(100*k/obj.Ng));
+                end
+            end
+            fprintf('\n')
+            
+            % Unweighted prior covariance
+            C = reshape(sum(obj.Ci,2),[obj.Nx, obj.Nx]);
+                
+            % Fix possible 0 diagonal elements
+            dc = diag(C);
+            dc(dc==0) = median(dc(dc~=0));
+            C = C - diag(diag(C)) + diag(dc);
+            
+            % Compute svd
+            sqC = chol(C);
+            [U,s] = svd(obj.H*sqC,'econ');
+            obj.s2 = diag(s).^2;
+            obj.Ut = U';
+            
+            if ~exist('compileInvChol','file')
+                addpath(fullfile(fileparts(which('RSBL')),'invChol'));
+            end
+            try
+                invChol_mex(eye(4));
+            catch
+                compileInvChol;
+            end
+        end
+        
+        %%
+        function [lambda, gamma, gamma_F, history] = learning(obj,Y, lambda0, gamma_F0, gamma0, options)
+            if nargin < 6
+                options = obj.defaultOptions;
+            end
+            if ~isfield(options,'doPruning')
+                options.doPruning = true;
+            end
+            %[lambda0, gamma_F0] = initHyperparameters(obj, y);
+            %UtY2 = (obj.Ut*Y).^2;
+            %gamma_F0 = (UtY2'-lambda0)*(1./(obj.s2+eps));
+            [lambda, gamma, gamma_F, history] = optimizeFullModel(obj,Y,lambda0, gamma_F0, options);
+            if options.doPruning
+                [gamma,history] = pruning(obj, Y, lambda, gamma, gamma0, history, options);
+            end
+        end
+        
+        %%
+        function Sw = getSw(obj)
+            Sw = reshape(obj.Ci*gamma,[obj.Nx obj.Nx]);
+        end
+        %%
+        function [x, lambda, gamma_F, gamma, logE, history] = update(obj,y,lambda, gamma0, options)
+            if nargin < 3
+                lambda = [];
+            end
+            if nargin < 4
+                gamma0 = eps;
+            end
+            if isempty(lambda)
+                [lambda, gamma_F0] = initHyperparameters(obj, y);
+            else
+                [~, gamma_F0] = initHyperparameters(obj, y);
+            end
+            if nargin < 5
+                options = obj.defaultOptions;
+            end
+            [lambda, gamma, gamma_F, history] = learning(obj, y, lambda, gamma_F0, gamma0, options);
+            logE = calculateLogEvidence(obj,y*y'/size(y,2),lambda,gamma);
+            
+            [~, iSy] = obj.calculateModelCov(lambda, gamma);
+            SxHt = reshape(obj.CiHt*gamma,[obj.Nx obj.Ny]);
+            x = SxHt*iSy*y;
+        end
+        %%
+        function ypred = predict(obj, x)
+            ypred = obj.H*x;
+        end
+        %%
+        function logE = calculateLogEvidence(obj,Cy,lambda,gamma, indices)
+            if nargin < 5, indices = 1:obj.Ng;end
+            [Sy, iSy] = calculateModelCov(obj, lambda, gamma, indices);
+            logE = (-1/2)*(trace(Cy*iSy) + RSBL.logDet(Sy));
+        end
+    end
+    methods(Access=private)
+        %%
+        function [lambda0, gamma0] = initHyperparameters(obj, Y)
+            UtY2 = (obj.Ut*Y).^2;
+            S = [obj.s2 obj.s2*0+1];
+            phi = abs(mean((S'*S)\(S'*UtY2),2));
+            gamma0  = phi(1);
+            lambda0 = phi(2);
+        end
+        %%
+        function [lambda, gamma, gamma_F, history] = optimizeFullModel(obj,Y,lambda0, gamma_F0, options)
+            UtY2 = (obj.Ut*Y).^2;
+            Nt = size(Y,2);
+            Cy = Y*Y'/Nt;
+            gamma = ones(obj.Ng,1);
+            lambda = lambda0;
+            gamma_F = gamma_F0;
+            gamma(:) = gamma_F;
+            
+            history = struct('lambda',nan(options.bufferSize,1),'gamma_F',nan(options.bufferSize,1),'logE',nan(options.bufferSize,1),'pointer',1);
+            history.lambda(1)  = lambda;
+            history.gamma_F(1) = gamma_F;
+            history.logE(1)    = calculateLogEvidence(obj,Cy,lambda,gamma);
+            
+            for k=2:options.maxIter
+                psi = gamma_F*obj.s2+lambda;
+                psi2 = psi.^2;
+                
+                %lambda   = (lambda0+lambda *(sum(mean(bsxfun(@times,UtY2,     1./psi2),2)))/(eps+sum(     1./psi)))/2;
+                gamma_F  = (gamma_F0+gamma_F*sum(mean(bsxfun(@times,UtY2,obj.s2./psi2),2))/(eps+sum(obj.s2./psi)))/2;
+                %lambda   = lambda *sum(mean(bsxfun(@times,UtY2,     1./psi2),2))/(eps+sum(     1./psi));
+                %gamma_F  = gamma_F*sum(mean(bsxfun(@times,UtY2,obj.s2./psi2),2))/(eps+sum(obj.s2./psi));
+                gamma(:) = gamma_F;
+                history.lambda(k) = lambda;
+                history.gamma_F(k) = gamma_F;
+                history.logE(k) = calculateLogEvidence(obj,Cy,lambda,gamma);
+                
+                if options.verbose
+                    fprintf('%i => diff(logE): %.4g   logE: %.5g   Lambda: %.4g   Gamma: %.4g\n',...
+                        k,abs(diff(fliplr(history.logE(k-1:k)))),history.logE(k),lambda,gamma_F);
+                end
+                
+                % Check convergence and exit condition
+                if diff(history.logE(k-1:k)) < options.maxTol, break;end
+            end
+            history.pointer = k;
+        end
+
+        %%
+        function [gamma, history] = pruning(obj,Y, lambda, gamma, gamma0, history, options)
+            % Implements the \gamma-MAP SBL algorithm of David Wipf and B.
+            % Rao.
+            %
+            %% -------------------------------------------------------------
+            % For certain problems the following algorithm may be faster:
+            %
+            %--Precompute the following matrices in the constructor (once)--
+            % obj.A = sparse([]);
+            % for i=1:obj.Ng
+            %     obj.A = blkdiag(obj.A,obj.Hi{i}');
+            % end
+            % obj.Ig = speye(obj.Ng);
+            %
+            %--Substitute this function with the following code-------------
+            % Nt = size(Y,2); 
+            % iSy_sq = sqrtm(iSy);
+            % Yk = kron(obj.Ig,iSy_sq'*Y);
+            % B = kron(obj.Ig,iSy_sq);
+            % C = obj.A*B;
+            % A = sum(reshape(sum((C*Yk).^2),Nt,obj.Ng))';
+            % B = sum(reshape(sum(C.^2), obj.Ny, obj.Ng))';
+            % gamma = (gamma0+gamma.*(A./B))/2;
+            %% -----------------------------------------------------------------------------
+            if numel(gamma0)==1
+                gamma0 = gamma*0+gamma0;
+            end
+            Nt = size(Y,2);
+            Cy = Y*Y'/Nt;
+            A = 0*gamma;
+            B = A;
+            for k=1:options.maxIter
+                [~, iSy] = obj.calculateModelCov(lambda,gamma);
+                for i=1:obj.Ng
+                    Hi_iSy = obj.Hi{i}'*iSy;
+                    A(i) = norm(Hi_iSy*Y,'fro')^2;
+                    B(i) = (abs(sum(sum((Hi_iSy)'.*obj.Hi{i}))));
+                end
+                gamma = 0.5*gamma0 + 0.5*gamma.*(A./B);
+                history.pointer = history.pointer+1;
+                history.logE(history.pointer) = calculateLogEvidence(obj,Cy,lambda,gamma);
+                if options.verbose
+                    fprintf('%i => diff(logE): %.4g   logE: %.5g   Sum Gamma: %.4g\n',history.pointer,diff(...
+                        history.logE(history.pointer-1:history.pointer)),history.logE(history.pointer),sum(nonzeros(gamma)));
+                end
+                if diff(history.logE(history.pointer-1:history.pointer)) < options.maxTol, break;end
+            end
+        end
+        
+        %%
+        function [Sy, iSy] = calculateModelCov(obj,lambda,gamma, indices)
+            if nargin < 4, indices = 1:obj.Ng;end
+            gHHt = sum(bsxfun(@times, obj.HiHit(:,:,indices),permute(gamma(indices),[3 2 1])),3);
+            Sy = lambda*obj.Iy+gHHt;
+            try
+                iSy = invChol_mex(double(Sy));
+            catch ME
+                if ~strcmp(ME.identifier,'MATLAB:invChol_mex:iscomplex')    
+                    warning(ME.message);
+                end
+                if strcmp(ME.identifier,'MATLAB:invChol_mex:dpotrf:notposdef')
+                    warning('Possibly the data is rank deficient!')
+                end
+                [Utmp,S,Vtmp] = svd(Sy);
+                stmp = real(diag(S));
+                invS = 1./stmp;
+                invS(isinf(invS)) = 0;
+                iSy = Utmp*diag(invS)*Vtmp';
+            end
+        end
+    end
+    methods(Static)
+        %%
+        function log_d = logDet(S)
+            log_d = log(det(S));     
+            if isinf(log_d)
+                e = eig(S);
+                e(e<0) = eps;
+                log_d = sum(log(e));
+            end
+        end
+    end
+end
---
+++ pop_rsbl.m
@@ -0,0 +1,325 @@
+function EEG = pop_rsbl(EEG, saveFull, account4artifacts, src2roiReductionType, solverType)
+persistent solver
+
+if nargin < 1, error('Not enough input arguments.');end
+if nargin == 1
+    answer = inputdlg({'Save full PCD (true/false)','Account for artifacts (true/false)', 'Source ROI type (power, mpower, ksdensity, hist, sum, or mean)', 'Solver type (bsbl, loreta)'},'pop_pebp', 1, {'true', 'true','power','bsbl'});
+    if isempty(answer)
+        return;
+    else
+        saveFull = str2num(lower(answer{1})); %#ok
+        account4artifacts = str2num(lower(answer{2})); %#ok
+        src2roiReductionType = lower(answer{3});
+        solverType = lower(answer{4});
+    end
+end
+if ~islogical(saveFull)
+    disp('Invalid input for saveFull parameter, we will use the default value.')
+    saveFull= true;
+end
+account4artifacts = logical(account4artifacts);
+if isempty(account4artifacts)
+    disp('Invalid input for account4artifacts parameter, we will use the default value.')
+    account4artifacts= true;
+end
+if ~any(ismember({'ksdensity','hist','mean','sum','power','mpower'},src2roiReductionType))
+    src2roiReductionType = 'power';
+end
+if nargin < 5
+    solverType = 'bsbl';
+end
+if ~any(ismember({'bsbl','loreta'},solverType))
+    solverType = 'bsbl';
+end
+
+% Load the head model
+try
+    hm = headModel.loadFromFile(EEG.etc.src.hmfile);
+catch
+    warning('EEG.etc.src.hmfile seems to be corrupted or missing, to set it right next we will run >> EEG = pop_forwardModel(EEG)');
+    EEG = pop_forwardModel(EEG, headModel.getDefaultTemplateFilename(), [0.33 0.022 0.33], true);
+    try
+        hm = headModel.loadFromFile(EEG.etc.src.hmfile);
+    catch
+        errordlg('For the second time EEG.etc.src.hmfile seems to be corrupted or missing, try the command >> EEG = pop_forwardModel(EEG);');
+        return;
+    end
+end
+
+% Select channels
+labels_eeg = {EEG.chanlocs.labels};
+[~,loc] = intersect(lower(labels_eeg), lower(hm.labels),'stable');
+EEG = pop_select(EEG,'channel',loc);
+
+% Initialize the inverse solver
+% hm.K = -hm.K;
+if account4artifacts && exist('Artifact_dictionary.mat','file')
+    [H, Delta, blocks, indG, indV] = buildAugmentedLeadField(hm);
+else
+    norm_K = norm(hm.K);
+    H = hm.K/norm_K;
+    Delta = hm.L/norm_K;
+    % Delta = hm.L/43120/6;
+    H = bsxfun(@rdivide,H,sqrt(sum(H.^2)));
+    if size(H,2) == 3*size(hm.cortex.vertices,1)
+        Delta = kron(eye(3),Delta);
+        blocks = hm.indices4Structure(hm.atlas.label);
+        blocks = logical(kron(eye(3),blocks));
+    end
+    indG = (1:size(H,2))';
+    indV = [];
+end
+Nx = size(H,2);
+if isempty(solver)
+    solver = RSBL(H, Delta, blocks);
+else
+    try
+        if sum((solver.H(:) - H(:)).^2) + sum((solver.Delta(:) - Delta(:)).^2) + sum((solver.Blocks(:) - blocks(:)).^2) ~=0
+            solver = RSBL(H, Delta, blocks); 
+        end
+    catch ME
+        if ~strcmp(ME.identifier,'MATLAB:dimagree')
+            disp(ME);
+        end
+        solver = RSBL(H, Delta, blocks);
+    end
+end
+solver.defaultOptions.verbose = false;
+if strcmp(solverType,'loreta')
+    solver.defaultOptions.doPruning = false;
+end
+EEG.data = double(EEG.data);
+Nroi = length(hm.atlas.label);
+
+% Allocate memory
+X = allocateMemory([Nx, EEG.pnts, EEG.trials]);
+X_roi = zeros(Nroi, EEG.pnts, EEG.trials);
+
+prc_5 = round(linspace(1,EEG.pnts,30));
+iterations = 1:5:EEG.pnts;
+prc_10 = iterations(round(linspace(1,length(iterations),10)));
+
+logE = zeros([EEG.pnts,EEG.trials]);
+lambda = zeros([EEG.pnts,EEG.trials]);
+gamma_F = zeros([EEG.pnts,EEG.trials]);
+gamma = zeros([solver.Ng,EEG.pnts,EEG.trials]);
+E = EEG.data*0;
+
+I = speye(Nx);
+B = -sign(Delta);
+B = B-diag(diag(B));
+B = -bsxfun(@rdivide, B,(sum(B,2)+eps));
+B = B+speye(Nx);
+A = (0.7*I-0.3*B);
+
+% Determine noise level
+n = 2*round(EEG.srate/2);
+Y = fft(EEG.data,n, 2);
+Y = Y(:,2:n/2+1,:);
+D = 1./(1:n/2);
+lambda0 = D'*(D'\mean(abs(Y),3)');
+lambda0 = mean(mean(lambda0(end-round(n/2/3):end,:)));
+fprintf('Approximated noise level: %f\n', lambda0);
+
+
+% Perform source estimation
+fprintf('PEB source estimation...\n');
+for trial=1:EEG.trials
+    fprintf('Processing trial %i of %i...',trial, EEG.trials);
+    
+    [X(:,1,trial), lambda(1,trial),gamma_F(1,trial),gamma(:,1,trial), logE(1,trial)] = solver.update(EEG.data(:,1,trial), lambda0);
+    itnumb = 0;
+    for k=2:EEG.pnts
+        
+        % Prediction
+        Xpred = A*X(:,k-1,trial);
+        e = EEG.data(:,k,trial) - solver.predict(Xpred);
+        E(:,k,trial) = e;
+              
+        % Source estimation
+        [dX, lambda(k,trial),gamma_F(k,trial),gamma(:,k,trial), logE(k,trial), history] = solver.update(e, lambda(k-1,trial), gamma(:,k-1,trial));
+        X(:,k,trial) = Xpred + dX;
+        
+        % Progress indicatior
+        if any(prc_5==k)
+            fprintf('.');
+        end
+        prc = find(prc_10==k);
+        if ~isempty(prc), fprintf('%i%%',prc*10);end
+    end
+    itnumb = itnumb + history.pointer;
+    fprintf('  %f', itnumb/EEG.pnts);
+    
+    % Compute average ROI time series
+    X_roi(:,:,trial) = computeSourceROI(X, hm, indG, trial, src2roiReductionType);
+    
+    % Data cleaning
+    EEG.data(:,:,trial) = cleanData(H, X, indG, trial);
+    fprintf('\n');
+end
+EEG.etc.src.act = X_roi;
+EEG.etc.src.roi = hm.atlas.label;
+EEG.etc.src.lambda = lambda;
+EEG.etc.src.gamma = gamma;
+EEG.etc.src.H = H;
+EEG.etc.src.indG = indG;
+EEG.etc.src.indV = indV;
+EEG.etc.src.logE = logE;
+EEG.etc.src.solver = solver;
+fprintf('done\n');
+
+if saveFull
+    try
+        EEG.etc.src.actFull = X;
+    catch
+        EEG.etc.src.actFull = invSol.LargeTensor([Nx, EEG.pnts, EEG.trials], tempname);
+        EEG.etc.src.actFull(:) = X(:);
+    end
+else
+    EEG.etc.src.actFull = [];
+end
+EEG.history = char(EEG.history,['EEG = pop_pep(EEG, ' num2str(saveFull) ', ' num2str(account4artifacts) ');']);
+disp('The source estimates were saved in EEG.etc.src');
+end
+
+
+%%
+function y = cleanData(H, X, indG, trial)
+try
+    y = H(:, indG)*X(indG,:, trial);
+catch
+    n = size(X,2);
+    y = zeros(size(H,1),n);
+    delta = min([1024 round(n/100)]);
+    for k=1:delta:n
+        ind = k:k+delta-1;
+        ind(ind>n) = [];
+        y(:,ind) = H(:, indG)*X(indG,ind, trial);
+    end
+end
+end
+
+%%
+function x_roi = computeSourceROI(X, hm, indG, trial, src2roiReductionType)
+% Construct the sum and average ROI operator
+T = hm.indices4Structure(hm.atlas.label);
+T = double(T)';
+P = sparse(bsxfun(@rdivide,T, sum(T,2)));
+
+% Find if we need to integrate over Jx, Jy, Jz components
+isVect = length(indG) == 3*size(hm.cortex.vertices,1);
+if isVect
+    P = [P P P]/3;
+    T = [T T T];
+end
+Nt = size(X,2);
+Nroi = size(P,1);
+x_roi = zeros(Nroi,Nt);
+blocks = P~=0;
+if strcmp(src2roiReductionType,'mean')
+    if isVect
+        warning('In a solution with (x,y,z) components, the ROI ''mean'' may not make a lot of sense, consider using the ''mpower'' (mean power) option, which is equivalent to taking the mean of dipole magnitudes.');
+    end
+    try
+        x = X(indG,:, trial);
+        x_roi = P*x;
+    catch
+        delta = min([1024 round(Nt/100)]);
+        for k=1:delta:Nt
+            ind = k:k+delta-1;
+            ind(ind>Nt) = [];
+            x_roi(:,ind) = P*X(indG,ind, trial);
+        end
+    end
+elseif strcmp(src2roiReductionType,'sum')
+    if isVect
+        warning('In a solution with (x,y,z) components, the ROI ''sum'' may not make a lot of sense, consider using the ''power'' (total power), which takes the sum of dipole magnitudes.');
+    end
+    try
+        x = X(indG,:, trial);
+        x_roi = T*x;
+    catch
+        delta = min([1024 round(Nt/100)]);
+        for k=1:delta:Nt
+            ind = k:k+delta-1;
+            ind(ind>Nt) = [];
+            x_roi(:,ind) = T*X(indG,ind, trial);
+        end
+    end
+elseif strcmp(src2roiReductionType,'power')
+    try
+        x = X(indG,:, trial);
+        x_roi = sqrt(T*(x.^2));
+    catch
+        delta = min([1024 round(Nt/100)]);
+        for k=1:delta:Nt
+            ind = k:k+delta-1;
+            ind(ind>Nt) = [];
+            if isVect
+                x_roi(:,ind) = sqrt(T*(X(indG,ind, trial).^2));
+            else
+                x_roi(:,ind) = T*(X(indG,ind, trial).^2);
+            end
+        end
+    end
+ elseif strcmp(src2roiReductionType,'mpower')
+    try
+        x = X(indG,:, trial);
+        x_roi = sqrt(P*(x.^2));
+    catch
+        delta = min([1024 round(Nt/100)]);
+        for k=1:delta:Nt
+            ind = k:k+delta-1;
+            ind(ind>Nt) = [];
+            if isVect
+                x_roi(:,ind) = sqrt(P*(X(indG,ind, trial).^2));
+            else
+                x_roi(:,ind) = P*(X(indG,ind, trial).^2);
+            end
+        end
+    end
+elseif strcmp(src2roiReductionType,'ksdensity')
+    delta = 5;
+    for r=1:Nroi
+        for k=1:delta:Nt
+            ind = k:k+delta-1;
+            ind(ind>Nt) = [];
+            if isVect
+                xv = abs(X(indG(blocks(r,:)),ind, trial));
+            else
+                xv = X(indG(blocks(r,:)),ind, trial);
+            end
+            [fx, xi] = ksdensity(xv(:));
+            Px = griddedInterpolant(xi,fx/sum(fx));
+            x_roi(r,ind) = sum(reshape(xv(:).*Px(xv(:)),size(xv)));
+        end
+    end
+elseif strcmp(src2roiReductionType,'hist')
+    delta = 5;
+    for r=1:Nroi
+        for k=1:delta:Nt
+            ind = k:k+delta-1;
+            ind(ind>Nt) = [];
+            if isVect
+                xv = abs(X(indG(blocks(r,:)),ind, trial));
+            else
+                xv = X(indG(blocks(r,:)),ind, trial);
+            end
+            [fx, xi] = hist(xv(:),100);
+            Px = griddedInterpolant(xi,fx/sum(fx));
+            x_roi(r,ind) = sum(reshape(xv(:).*Px(xv(:)),size(xv)));
+        end
+    end
+end
+end
+
+%%
+function X = allocateMemory(dim)
+try
+    X = zeros(dim);
+catch ME
+    disp(ME.message)
+    disp('Using a LargeTensor object...')
+    X = LargeTensor(dim);
+end
+end
